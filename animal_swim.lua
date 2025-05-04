e13cc57d8cd0922ed1027a000bf990d161cb084c
Thomthom160 <89802398+Thomthom160@users.noreply.github.com>
2025-05-04 19:14:22 +0200

    • Fix variable naming inconsistencies • Replace hardcoded values with variables • Update event handling for ped type changes • Add lib.onCache call for ped registration

--- animal_swim.lua
+++ animal_swim.lua
@@ -1,69 +1,60 @@
-local QBCore = exports['qb-core']:GetCoreObject()
-
-local function IsPlayerAnimal()
-    local ped = PlayerPedId()
-    return GetPedType(ped) == 28
-end
+local isPlayerAnimal = false
 
 local function HandleAnimalSwim()
-    local ped = PlayerPedId()
     print("^6Debug^7: ^3Animal Swim ^2module ^4loaded")
 
     CreateThread(function()
-        while true do
+        while isPlayerAnimal do
             Wait(200)
 
-            if not IsPlayerAnimal() then break end
+            SetPedDiesInWater(cache.ped, false)
+            SetPedDiesInstantlyInWater(cache.ped, false)
 
-            SetPedDiesInWater(ped, false)
-            SetPedDiesInstantlyInWater(ped, false)
+            if IsEntityInWater(cache.ped) and not IsPedInAnyVehicle(cache.ped, false) then
+                SetPedCanRagdoll(cache.ped, false)
 
-            if IsEntityInWater(ped) and not IsPedInAnyVehicle(ped, false) then
-                SetPedCanRagdoll(ped, false)
-
-                local maxHealth = GetEntityMaxHealth(ped)
-                if GetEntityHealth(ped) < maxHealth then
-                    SetEntityHealth(ped, maxHealth)
+                local maxHealth = GetEntityMaxHealth(cache.ped)
+                if GetEntityHealth(cache.ped) < maxHealth then
+                    SetEntityHealth(cache.ped, maxHealth)
                 end
 
-                local submergedLevel = GetEntitySubmergedLevel(ped)
-                local pedCoords = GetEntityCoords(ped)
-                local _, waterHeight = GetWaterHeight(pedCoords.x, pedCoords.y, pedCoords.z)
-                local waterDepth = waterHeight - pedCoords.z
+                local submergedLevel = GetEntitySubmergedLevel(cache.ped)
+                local _, waterHeight = GetWaterHeight(cache.coords.x, cache.coords.y, cache.coords.z)
+                local waterDepth = waterHeight - cache.coords.z
 
                 if submergedLevel > 0.25 then
                     if waterDepth <= 0.5 then
-                        ApplyForceToEntity(ped, 1, 0.0, 0.0, 0.6, 0.0, 0.0, 0.0, 0, true, true, true, false, true)
+                        ApplyForceToEntity(cache.ped, 1, 0.0, 0.0, 0.6, 0.0, 0.0, 0.0, 0, true, true, true, false, true)
                     else
                         local force = math.min(0.4 * (submergedLevel - 0.25), 0.2)
-                        ApplyForceToEntity(ped, 1, 0.0, 0.0, force, 0.0, 0.0, 0.0, 0, true, true, true, false, true)
+                        ApplyForceToEntity(cache.ped, 1, 0.0, 0.0, force, 0.0, 0.0, 0.0, 0, true, true, true, false, true)
                     end
 
-                    if pedCoords.z > waterHeight + 0.1 then
-                        ApplyForceToEntity(ped, 1, 0.0, 0.0, -0.1, 0.0, 0.0, 0.0, 0, true, true, true, false, true)
+                    if cache.coords.z > waterHeight + 0.1 then
+                        ApplyForceToEntity(cache.ped, 1, 0.0, 0.0, -0.1, 0.0, 0.0, 0.0, 0, true, true, true, false, true)
                     end
 
-                    if pedCoords.z < waterHeight - 0.1 then
-                        ApplyForceToEntity(ped, 1, 0.0, 0.0, 0.2, 0.0, 0.0, 0.0, 0, true, true, true, false, true)
+                    if cache.coords.z < waterHeight - 0.1 then
+                        ApplyForceToEntity(cache.ped, 1, 0.0, 0.0, 0.2, 0.0, 0.0, 0.0, 0, true, true, true, false, true)
                     end
 
-                    local currentRotation = GetEntityRotation(ped, 2)
-                    SetEntityRotation(ped, currentRotation.x + 0.2, currentRotation.y, currentRotation.z, 2, true)
+                    local currentRotation = GetEntityRotation(cache.ped, 2)
+                    SetEntityRotation(cache.ped, currentRotation.x + 0.2, currentRotation.y, currentRotation.z, 2, true)
                 end
             else
-                SetPedCanRagdoll(ped, true)
+                SetPedCanRagdoll(cache.ped, true)
             end
         end
     end)
 end
 
-RegisterNetEvent('QBCore:Client:OnPlayerLoaded', function()
-    print("^6Debug^7: ^2Loading ^3Animal Swim ^2module^7...")
-    Wait(3000)
 
-    if IsPlayerAnimal() then
+lib.onCache('ped', function(val, oldVal)
+    Wait(3000)
+    if GetPedType(val) == 28 then
+        isPlayerAnimal = true
         HandleAnimalSwim()
     else
-        print("^6Debug^7: ^2Player is ^1not ^2animal^7, ^2stopping ^3Animal Swim ^2module^7...")
+        isPlayerAnimal = false
     end
 end)
--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,4 +5,5 @@ author 'Cloutmatic'
 description 'Animal Swimming Script'
 version '1.0.0'
 
+shared_script '@ox_lib/init.lua'
 client_script 'animal_swim.lua'
